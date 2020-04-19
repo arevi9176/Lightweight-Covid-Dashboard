df1759bd8f84551d38e542ef6832a7db72ae4961
apollon67 <apollon67@gmx.de>
2020-04-19 23:12:08 +0200

    added trend in view 'Average growth rate in the last seven days'

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 <img align="right" src="images/lcd_sh01.png?raw=true">
 There are many good and sophisticated dashboards out there, nevertheless I missed the possibility to compare the current situation in different countries at a glance, especially on mobile devices.
 
-The underlying data comes from the Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE) - data is updated once a day.
+These are the available views:
+- Total Confirmed Cases
+- Daily New Cases
+- Total Deaths
+- Daily New Deaths
+- Average growth rate in the last seven days
 
-The data on the number of residents of the respective countries comes from the World Bank (last update from 2018). The data on the number of residents per case is only a rough indication and will vary considerably locally!
+The underlying data comes from the Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE) - data is updated once a day. The data on the number of residents of the respective countries comes from the World Bank (last update from 2018). The data on the number of residents per case is only a rough indication and will vary considerably locally!
 
 At the time of writing (March 2020) many countries are in the state of exponential growth or about to enter this phase.
 Only a few countries, e.g. China and South Korea, seem to have almost halted the further spread of the virus.
--- lcd.py
+++ lcd.py
@@ -5,8 +5,9 @@
 # 02.04.20 (v0.2.0) - added dataframe cache and view 'Average percentage increase in the last seven days'
 # 03.04.20 (v0.2.1) - changed 'Average percentage increase' to 'Average growth rate'
 # 15.04.20 (v0.3.0) - totally refactored and some bug fixes
+# 19.04.20 (v0.4.0) - added trend in view 'Average growth rate in the last seven days'
 #
-__version__ = "v0.3.0"
+__version__ = "v0.4.0"
 
 import pandas
 import urllib.request
@@ -14,12 +15,14 @@ import urllib.request
 from flask import Flask, render_template, request
 from time import time as time
 from lcd_cnpop import get_country_population
-from math import log10 as log10
+from math import log10, isinf, isnan
 
 CONFIRMED_GLOBAL_URL = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 CONFIRMED_GLOBAL_FILE = "time_series_covid19_confirmed_global.csv"
 DEATHS_GLOBAL_URL = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 DEATHS_GLOBAL_FILE = "time_series_covid19_deaths_global.csv"
+RECOVERED_GLOBAL_URL = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+RECOVERED_GLOBAL_FILE = "time_series_covid19_recovered_global.csv"
 
 class CovidData:
     def __init__(self, filepath, url):
@@ -55,6 +58,19 @@ class CovidData:
         df.insert(1, 'Residents per case', residents_per_case)
         return df
 
+    def _insert_trend_column(self, df, distance):
+        '''
+        Insert trend column at pos 2 in dataframe.
+        Trend means percentage rate of increase of two consecutive periods described by <distance> in days.
+        '''
+        diff1 = df[df.columns[-1]] - df[df.columns[-(1 + distance)]]
+        diff2 = df[df.columns[-(1 + distance)]] - df[df.columns[-(1 + 2 * distance)]]
+        quot = diff1 / diff2
+        quot = -(1 - quot) * 100
+        ql = [round(x) if not (isinf(x) or isnan(x)) else "n/a at this time" for x in list(quot)]
+        df.insert(2, 'Trend', ql)
+        return df
+
     def _drop_rows_based_on_unique_indicis(self, df_source, df_target):
         '''Drop every row in df_target where the row index is not contained in df_source'''
         set_of_source_indicis = set(df_source.index)
@@ -85,14 +101,16 @@ class CovidData:
         Relative numbers (percent values) are used for length of bargraphs.
         Negative numbers are set to zero!
         '''
-        residents_per_case = list(df['Residents per case'])                 # save column
-        population = list(df['Population'])                                 # save column
-        df_tmp = df.drop(['Population','Residents per case'], axis=1)       # delete columns from dataframe
-        max_value = df_tmp.max().max()                                      # compute max value
-        df_rel = df_tmp.div(max_value).multiply(100).round(1)               # compute relative values
-        df_rel[df_rel < 0] = 0                                              # set negative numbers to 0
-        df_rel.insert(0, 'Population', population)                          # restore column
-        df_rel.insert(1, 'Residents per case', residents_per_case)          # restore column
+        residents_per_case = list(df['Residents per case'])                     # save column
+        population = list(df['Population'])                                     # save column
+        trend = list(df['Trend'])                                               # save column
+        df_tmp = df.drop(['Population','Residents per case', 'Trend'], axis=1)  # delete columns from dataframe
+        max_value = df_tmp.max().max()                                          # compute max value
+        df_rel = df_tmp.div(max_value).multiply(100).round(1)                   # compute relative values
+        df_rel[df_rel < 0] = 0                                                  # set negative numbers to 0
+        df_rel.insert(0, 'Population', population)                              # restore column
+        df_rel.insert(1, 'Residents per case', residents_per_case)              # restore column
+        df_rel.insert(2, 'Trend', trend)                                        # restore column        
         return df_rel
 
     def _compute_abs_dataframe(self):
@@ -116,18 +134,20 @@ class CovidData:
 
     def _compute_agr_dataframe(self, df):
         '''Compute dataframe with average growth rate, sorted by last column.'''
-        residents_per_case = list(df['Residents per case'])                 # save column
-        population = list(df['Population'])                                 # save column
-        df_tmp = df.drop(['Population','Residents per case'], axis=1)       # delete columns from dataframe
-        df_agr = df_tmp.pct_change(axis=1)                                  # compute percentage change
-        df_agr = df_agr.drop(df_agr.columns[0], axis=1)                     # first column becomes NaN -> drop
-        si_pct = df_agr.mean(axis=1)                                        # compute mean values (returns series, not dataframe!)
-        si_pct = si_pct.multiply(100).round(1)                              # adjust 0.1234 -> 12.3
-        df_agr.insert(0, 'Population', population)                          # restore column
-        df_agr.insert(1, 'Residents per case', residents_per_case)          # restore column
-        df_agr.insert(2, 'AGR (mean value)', list(si_pct))                  # save mean values
-        df_agr = df_agr.iloc[:, list(range(3))]                             # drop evrything after column 'Mean Values'
-        df_agr = df_agr.sort_values(df_agr.columns[-1], ascending = False)  # sort by 'Mean Values'
+        residents_per_case = list(df['Residents per case'])                     # save column
+        population = list(df['Population'])                                     # save column
+        trend = list(df['Trend'])                                               # save column
+        df_tmp = df.drop(['Population','Residents per case', 'Trend'], axis=1)  # delete columns from dataframe
+        df_agr = df_tmp.pct_change(axis=1)                                      # compute percentage change
+        df_agr = df_agr.drop(df_agr.columns[0], axis=1)                         # first column becomes NaN -> drop
+        si_pct = df_agr.mean(axis=1)                                            # compute mean values (returns series, not dataframe!)
+        si_pct = si_pct.multiply(100).round(1)                                  # adjust 0.1234 -> 12.3
+        df_agr.insert(0, 'Population', population)                              # restore column
+        df_agr.insert(1, 'Residents per case', residents_per_case)              # restore column
+        df_agr.insert(2, 'Trend', trend)                                        # restore column
+        df_agr.insert(3, 'AGR (mean value)', list(si_pct))                      # save mean values
+        df_agr = df_agr.iloc[:, list(range(4))]                                 # drop evrything after column 'Mean Values'
+        df_agr = df_agr.sort_values(df_agr.columns[-1], ascending = False)      # sort by 'Mean Values'
         return df_agr
 
     def _check_for_update(self):
@@ -139,19 +159,22 @@ class CovidData:
             urllib.request.urlretrieve(self.url, self.filepath)
             self.df_abs = self._compute_abs_dataframe()
             self.df_dif = self._compute_dif_dataframe()
-            # Insert population column to dataframes
-            self.df_abs = self._insert_pop_column( self.df_abs)
-            self.df_dif = self._insert_pop_column( self.df_dif)
-            # Insert residents per case column at pos 1 in dataframe.
+            # Insert population column at pos 0 in dataframe
+            self.df_abs = self._insert_pop_column(self.df_abs)
+            self.df_dif = self._insert_pop_column(self.df_dif)
+            # Insert residents per case column at pos 1 in dataframe
             self.df_abs = self._insert_residents_per_case_column(self.df_abs)
             self.df_dif = self._insert_residents_per_case_column(self.df_dif)
+            # Insert trend column at pos 2 in dataframe
+            self.df_abs = self._insert_trend_column(self.df_abs, 6)
+            self.df_dif = self._insert_trend_column(self.df_dif, 6)
             self.last_update_time = time()
 
     def _prune_dataframe(self, df, cases, days):
         '''
         Discard all columns before <days> and all rows with less than <cases> in last column but only if cases > 0.
         '''
-        df = df.iloc[:, [0, 1] + list(range(-days, 0))]
+        df = df.iloc[:, [0, 1, 2] + list(range(-days, 0))]
         if cases > 0:
             last_column = list(df.columns)[-1]
             df = df[df[last_column] >= cases]
@@ -189,7 +212,7 @@ class CovidData:
         df_rel_tmp = self._compute_rel_dataframe(df_abs_tmp)
         abs_values = df_abs_tmp.reset_index().values.tolist()
         rel_values = df_rel_tmp.reset_index().values.tolist()
-        dat_start = df_abs_tmp.columns[2]
+        dat_start = df_abs_tmp.columns[3]
         dat_end = df_abs_tmp.columns[-1]
         return abs_values, rel_values, dat_start, dat_end
 
@@ -202,7 +225,7 @@ class CovidData:
         df_rel_tmp = self._compute_rel_dataframe(df_dif_tmp)
         dif_values = df_dif_tmp.reset_index().values.tolist()
         rel_values = df_rel_tmp.reset_index().values.tolist()
-        dat_start = df_abs_tmp.columns[2]
+        dat_start = df_abs_tmp.columns[3]
         dat_end = df_abs_tmp.columns[-1]
         return dif_values, rel_values, dat_start, dat_end
 
@@ -214,12 +237,13 @@ class CovidData:
         df_agr_tmp = self._append_double_rates_to_df_agr(df_agr_tmp)
         agr_values = df_agr_tmp.reset_index().values.tolist()
         rel_values = df_rel_tmp.reset_index().values.tolist()
-        dat_start = df_abs_tmp.columns[2]
+        dat_start = df_abs_tmp.columns[3]
         dat_end = df_abs_tmp.columns[-1]
         return agr_values, rel_values, dat_start, dat_end
 
 coviddata_confirmed = CovidData(CONFIRMED_GLOBAL_FILE, CONFIRMED_GLOBAL_URL)
 coviddata_deaths = CovidData(DEATHS_GLOBAL_FILE, DEATHS_GLOBAL_URL) 
+coviddata_recovered = CovidData(RECOVERED_GLOBAL_FILE, RECOVERED_GLOBAL_URL) 
 
 app = Flask(__name__)
 
@@ -263,6 +287,26 @@ def deaths_dif():
     context = ["deaths_dif", header, days, cases, "deaths", country, __version__]
     return render_template('lcd.html', title='deaths (differential)', absval=absval, relval=relval, context=context)
 
+@app.route("/recovered/")
+def recovered():
+    days = request.args.get('days', default=7, type=int)
+    cases = request.args.get('cases', default=100, type=int)
+    country = request.args.get('country', default="", type=str)
+    absval, relval, dat_start, dat_end = coviddata_recovered.compute_abs_values(days, cases, country)
+    header = "Total Recovered (" + dat_start + ' - ' + dat_end + ').'
+    context = ["recovered", header, days, cases, "recovered", country, __version__] 
+    return render_template('lcd.html', title='recovered', absval=absval, relval=relval, context=context)
+
+@app.route("/recovered_dif/")
+def recovered_dif():
+    days = request.args.get('days', default=7, type=int)
+    cases = request.args.get('cases', default=100, type=int)
+    country = request.args.get('country', default="", type=str)
+    absval, relval, dat_start, dat_end = coviddata_recovered.compute_dif_values(days, cases, country)
+    header = "Daily New Recovered (" + dat_start + ' - ' + dat_end + ').'
+    context = ["recovered_dif", header, days, cases, "recovered", country, __version__]
+    return render_template('lcd.html', title='recovered (differential)', absval=absval, relval=relval, context=context)
+
 @app.route("/av_growth_rate/")
 def average_percentage_increase():
     days = request.args.get('days', default=7, type=int)
@@ -273,5 +317,4 @@ def average_percentage_increase():
     return render_template('lcd.html', title='Average growth rate in the last seven days', absval=absval, relval=relval, context=context)
 
 if __name__ == '__main__':
-    #app.run(debug=True)
     app.run(host='0.0.0.0', debug=True)
